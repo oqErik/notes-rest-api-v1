9fbcde5ad225c6da6090ba03b039c3f32700ffb6
ERIK ORTIZ <oqerik@gmail.com>
2022-03-27 17:23:58 -0600

    example .env created, updated some code

--- controllers/search.js
+++ controllers/search.js
@@ -21,13 +21,15 @@ const allUsers = async ( req, res = response ) => {
 const searchUsers = async ( req, res = response ) => {
     try {
         const { term } = req.params;
+        //if mongo id is sent then they want a specific user
         const isMongoID = ObjectId.isValid( term );
-
         if ( isMongoID ) {
             const user = await Usuario.findById( term )
             if ( !user ) return res.status( 401 ).json( { msg: 'user not found' } );
             return res.status( 200 ).json( user );
         }
+
+        //if not means the put a query to search
         const regex = new RegExp( term, 'i' );
         const users = await Usuario.find( { $or: [ { name: regex }, { email: regex } ] } );
 
@@ -58,23 +60,23 @@ const allNotes = async ( req, res = response ) => {
 const searchNotes = async ( req, res = response ) => {
     try {
         const { term } = req.params;
+        //if mongo id is sent then they want a specific note
         const isMongoID = ObjectId.isValid( term );
-
         if ( isMongoID ) {
             const note = await Note.findById( term )
             if ( !note ) return res.status( 401 ).json( { msg: 'note not found' } );
             return res.status( 200 ).json( note );
         }
 
+        //if not means the put a query to search
         let notes = await Note.find().populate( 'user', [ 'name', 'email' ] );
-
-
         const regex = new RegExp( term, 'i' );
+
         notes = notes.filter( ( item ) => {
             if ( regex.test( item.title ) || regex.test( item.description ) || regex.test( item.user.name ) ) return item;
         } )
 
-        res.json( { total: notes.length, notes } )
+        res.status( 200 ).json( { total: notes.length, notes } )
     } catch ( error ) {
         console.warn( error );
         res.status( 500 );
--- models/server.js
+++ models/server.js
@@ -52,7 +52,7 @@ class Server {
 
 	listen() {
 		this.app.listen( this.port, () => {
-			console.log( `Server runnin on port:  ${this.port}` );
+			console.log( `Server runnin on http://localhost:${this.port}` );
 		} );
 	}
 }
